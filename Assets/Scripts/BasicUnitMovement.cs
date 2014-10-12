961e9098f2ec76a864bfe727dfa99da3ec26faac
wormangel <wormangel@gmail.com>
2014-10-12 20:51:31 -0300

    basic unit movement

---
+++ Assets/Scripts/BasicUnitMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicUnitMovement : MonoBehaviour
+{
+	#region Public parameters
+	
+	public float speed = 10f;
+	
+	#endregion
+	
+	#region Private attributes
+	
+	#endregion
+	
+	
+	// Use this for initialization
+	void Start()
+	{
+		transform.position = new Vector3(0, 0, 0);
+		rigidbody2D.velocity = Vector2.right.normalized * speed;
+	}
+	
+}

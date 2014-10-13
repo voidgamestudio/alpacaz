dacda4ebd782fb0fdd9827577d33b92e1d79640c
wormangel <wormangel@gmail.com>
2014-10-12 23:20:53 -0300

    dunno

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+
+	public string targetTag;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		if (collisionInfo.collider.gameObject.tag == targetTag) {
+			Debug.Log("Oi " + targetTag);
+			rigidbody2D.velocity = Vector3.zero;
+			collisionInfo.collider.gameObject.rigidbody2D.velocity = Vector3.zero;
+		}
+	}
+
+}

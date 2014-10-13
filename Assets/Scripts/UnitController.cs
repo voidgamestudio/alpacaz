16b751ed5ed09df909068edc2ec1092834cb3358
wormangel <wormangel@gmail.com>
2014-10-13 00:02:46 -0300

    fight

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class UnitController : MonoBehaviour {
 
 	public string targetTag;
+	public float hp = 100;
+	private UnitState state;
+	private GameObject currentTarget;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +15,24 @@ public class UnitController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (state == UnitState.Attacking) {
+			CauseDamage(currentTarget);
+			CheckDeath(currentTarget);
+		}
+	}
 
+	void CauseDamage(GameObject target) {
+		float damage = Random.Range (0.0F, 10.0F);
+		UnitController targetController = (UnitController) currentTarget.GetComponent<UnitController>();
+		targetController.hp -= damage;
+		Debug.Log (targetTag + " received a damage of " + damage + ". Current HP: " + targetController.hp);
+	}
+
+	void CheckDeath(GameObject target) {
+		UnitController targetController = (UnitController) target.GetComponent<UnitController>();
+		if (targetController.hp <= 0.0F) {
+			Destroy(target);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collisionInfo)
@@ -21,7 +41,14 @@ public class UnitController : MonoBehaviour {
 			Debug.Log("Oi " + targetTag);
 			rigidbody2D.velocity = Vector3.zero;
 			collisionInfo.collider.gameObject.rigidbody2D.velocity = Vector3.zero;
+			state = UnitState.Attacking;
+			currentTarget = collisionInfo.collider.gameObject;
 		}
 	}
 
+}
+
+public enum UnitState {
+	Walking,
+	Attacking
 }

b8831559b57f157dfd9ba402942c35dc8ab39229
Nicholas Alexander <nickmaclewy222@gmail.com>
2014-10-12 23:26:06 -0300

    Adding button and GameController prefabs

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public GameObject Unit;
+    public GameControllerScript gc;
+
+    #region Private attributes
+
+    private int buttonSize = 64;
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicado!");
+        gc.CreateUnit(Unit);
+    }
+}
---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour
+{
+
+    public GameObject playerBase;
+
+    
+    #region private attributes
+
+    private float offset = 0.1f;
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreateUnit(GameObject unit)
+    {
+        Vector3 baseSize = playerBase.renderer.bounds.size;
+        Vector3 unitInitialPosition = playerBase.transform.position + new Vector3(baseSize.x + offset, baseSize.y);
+        Debug.Log(unitInitialPosition);
+        Instantiate(unit, unitInitialPosition, Quaternion.identity);
+    }
+}
